30281396b9495b2e1fd5b834c903a9a2509e5305
rafaelsrabelo10 <rafaelrabelodev@gmail.com>
2021-12-27 17:00:15 -0300

    relaçoes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  resources :mining_types
+  # get 'welcome/index'
+  # get '/inicio', to: 'welcome#index'
   resources :coins
+
+  root to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
